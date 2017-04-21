508320d35de333f0a26991677e8a88f3ca59be5c
Kara-wan <wansheng_ios@sina.com>
2017-04-21 15:41:47 +0800

    创建项目

---
+++ Pudge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pudge
+//
+//  Created by 万圣 on 2017/4/21.
+//  Copyright © 2017年 万圣. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
